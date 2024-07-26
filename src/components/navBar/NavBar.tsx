6b89714f3861052335b641932bbb6d33fdae7509
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-26 17:14:12 +0200

    Implement underline current Link

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -3,28 +3,50 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import DarkModeToggle from '../darkmode/DarkModeToggle';
 import ROUTES from '../../constants/routes';
+import { Link, useLocation } from 'react-router-dom';
+
+const NavItem = ({ route, name }: { route: string, name: string }) => (
+    <Nav.Item>
+        <Nav.Link
+            as={Link}
+            to={route}
+            eventKey={route}
+            style={{ color: 'white' }}
+        >{name}</Nav.Link>
+    </Nav.Item>
+);
 
 
-function PorfolioNavbar() {
+const NavbarLinks = () => {
+    const location = useLocation();
+
     return (
-        <Navbar expand="lg" bg="dark">
-            <Container>
-                <Navbar.Brand style={{color:'white'}} href={ROUTES.HOME}>adb-dev</Navbar.Brand>
-                <div  className="d-flex align-items-center">
-                    <DarkModeToggle />
-                    <Navbar.Toggle aria-controls='navbar-nav-toggle' />
-                </div>
-                <Navbar.Collapse id='navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link style={{color:'white'}} href={ROUTES.HOME}>Home</Nav.Link>
-                        <Nav.Link style={{color:'white'}} href={ROUTES.ABOUT}>About</Nav.Link>
-                        <Nav.Link style={{color:'white'}} href={ROUTES.RESUME}>Resume</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Navbar.Collapse id='navbar-nav'>
+            <Nav className='me-auto'
+                defaultActiveKey={ROUTES.HOME}
+                activeKey={location.pathname}
+                variant="underline"
+            >
+                <NavItem route={ROUTES.HOME} name='Home' />
+                <NavItem route={ROUTES.ABOUT} name='About' />
+                <NavItem route={ROUTES.RESUME} name='Resume' />
+            </Nav>
+        </Navbar.Collapse>
     );
 }
+
+const PorfolioNavbar = () => (
+    <Navbar expand="lg" bg="dark">
+        <Container>
+            <Navbar.Brand as={Link} style={{ color: 'white' }} to={ROUTES.HOME}>adb-dev</Navbar.Brand>
+            <div className="d-flex align-items-center">
+                <DarkModeToggle />
+                <Navbar.Toggle aria-controls='navbar-nav-toggle' />
+            </div>
+            <NavbarLinks />
+        </Container>
+    </Navbar>
+);
 
 
 export default PorfolioNavbar;
